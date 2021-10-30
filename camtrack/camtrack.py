1cfb3a39313973da17f9aa7cddf9adb7e40f2442
Your Name <you@example.com>
2021-10-30 18:59:08 +0300

    + init hw2

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -7,6 +7,8 @@ __all__ = [
 from typing import List, Optional, Tuple
 
 import numpy as np
+import cv2
+import sortednp as snp
 
 from corners import CornerStorage
 from data3d import CameraParameters, PointCloud, Pose
@@ -17,8 +19,18 @@ from _camtrack import (
     calc_point_cloud_colors,
     pose_to_view_mat3x4,
     to_opencv_camera_mat3x3,
-    view_mat3x4_to_pose
+    view_mat3x4_to_pose,
+    build_correspondences,
+    TriangulationParameters,
+    triangulate_correspondences,
+    rodrigues_and_translation_to_view_mat3x4
 )
+
+triang_params = TriangulationParameters(
+    max_reprojection_error=7.5,
+    min_triangulation_angle_deg=1.0,
+    min_depth=0.1)
+iterations = 108
 
 
 def track_and_calc_colors(camera_parameters: CameraParameters,
@@ -36,12 +48,128 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
         rgb_sequence[0].shape[0]
     )
 
+
     # TODO: implement
+
     frame_count = len(corner_storage)
     view_mats = [pose_to_view_mat3x4(known_view_1[1])] * frame_count
-    corners_0 = corner_storage[0]
-    point_cloud_builder = PointCloudBuilder(corners_0.ids[:1],
-                                            np.zeros((1, 3)))
+    point_cloud_builder = PointCloudBuilder()
+
+
+    known_ind1 = known_view_1[0]
+    known_ind2 = known_view_2[0]
+
+    view_mat1 = pose_to_view_mat3x4(known_view_1[1])
+    view_mat2 = pose_to_view_mat3x4(known_view_2[1])
+
+    view_mats[known_ind1] = view_mat1
+    view_mats[known_ind2] = view_mat2
+
+    frame1 = corner_storage[known_ind1]
+    frame2 = corner_storage[known_ind2]
+    
+    correspondence = build_correspondences(frame1, frame2)
+    pts3d, ids, cos_med = triangulate_correspondences(correspondence,
+                                                        view_mat1,
+                                                        view_mat2,
+                                                        intrinsic_mat,
+                                                        triang_params)
+
+    point_cloud_builder.add_points(ids, pts3d)
+    
+    used = [False] * frame_count
+    used[known_ind1] = True
+    used[known_ind2] = True
+
+
+    while True:
+        best_for_pnp = -1
+        inliers_for_best = None
+        r_vec_for_best = None
+        t_vec_for_best = None
+        ind1_for_best = None
+        ind2_for_best = None
+        corn_for_best = None
+
+        for i in range(frame_count):
+            if used[i]:
+                continue
+
+            corn = corner_storage[i]
+            inds, (ind1, ind2) = snp.intersect(point_cloud_builder.ids.flatten(), corn.ids.flatten(), indices=True)
+            
+            if len(inds) < 4:
+                continue
+
+            succ, r_vec, t_vec, inliers = cv2.solvePnPRansac(
+                point_cloud_builder.points[ind1],
+                corn.points[ind2],
+                intrinsic_mat,
+                np.array([]),
+                iterationsCount=iterations,
+                reprojectionError=triang_params.max_reprojection_error,
+                flags=cv2.SOLVEPNP_EPNP
+            )
+
+            if succ:
+                if best_for_pnp == -1:
+                    inliers_for_best = inliers
+                    r_vec_for_best = r_vec
+                    t_vec_for_best = t_vec
+                    best_for_pnp = i
+                    ind1_for_best = ind1
+                    ind2_for_best = ind2
+                    corn_for_best = corn
+
+                elif len(inliers_for_best) < len(inliers):
+                    inliers_for_best = inliers
+                    r_vec_for_best = r_vec
+                    t_vec_for_best = t_vec
+                    best_for_pnp = i
+                    ind1_for_best = ind1
+                    ind2_for_best = ind2
+                    corn_for_best = corn
+
+        #print(best_for_pnp)
+        if best_for_pnp == -1:
+            break
+            
+        succ, r_vec, t_vec = cv2.solvePnP(
+            point_cloud_builder.points[ind1_for_best[inliers_for_best.flatten()]],
+            corn_for_best.points[ind2_for_best[inliers_for_best.flatten()]],
+            intrinsic_mat,
+            np.array([]),
+            r_vec_for_best,
+            t_vec_for_best,
+            True,
+            cv2.SOLVEPNP_ITERATIVE
+        )
+
+        if not succ:
+            continue
+
+        view_mats[best_for_pnp] = rodrigues_and_translation_to_view_mat3x4(r_vec, t_vec)
+
+        best_pts3d = None
+        best_ids = None
+        cos_med_best = 1
+        for j in range(frame_count):
+            if not used[j]:
+                continue
+            correspondence = build_correspondences(corner_storage[best_for_pnp], corner_storage[j])
+            pts3d, ids, cos_med = triangulate_correspondences(correspondence,
+                                                                view_mats[best_for_pnp],
+                                                                view_mats[j],
+                                                                intrinsic_mat,
+                                                                triang_params)
+
+            if cos_med < cos_med_best:
+                best_pts3d = pts3d
+                best_ids = ids
+        
+        point_cloud_builder.add_points(best_ids, best_pts3d)
+        used[best_for_pnp] = True
+        
 
     calc_point_cloud_colors(
         point_cloud_builder,
