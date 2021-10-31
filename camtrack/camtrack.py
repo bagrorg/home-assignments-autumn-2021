29baa61a884bb12ad88de98216f465c5b62da4b1
Your Name <you@example.com>
2021-10-31 19:53:14 +0300

    + working variant with outliers

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -27,28 +27,20 @@ from _camtrack import (
     check_baseline
 )
 
-range_of_neighbours_frac = 1 / 10
-range_of_near = 2
+range_of_neighbours = 75
 
 triang_params = TriangulationParameters(
-    max_reprojection_error=7.5,
-    min_triangulation_angle_deg=1.0,
-    min_depth=0.1)
+    max_reprojection_error=6,
+    min_triangulation_angle_deg=1,
+    min_depth=0.11)
 iterations = 108
-base_line_min_dist = 0
-
+baseline_min_dist = 0
 
 def get_neighbours(i, frames):
-    l = max(0, i - frames * range_of_neighbours_frac / 2)
-    r = min(frames, i + frames * range_of_neighbours_frac / 2)
+    l = max(0, i - range_of_neighbours // 2)
+    r = min(frames, i + range_of_neighbours // 2)
 
     res = set(range(l, r))
-    return res
-
-def get_neighbours_without_near_range(i, frames):
-    l_b = max(0, i - range_of_near)
-    r_b = min(frames, i + range_of_near)
-    res = get_neighbours(i, frames) - set(range(l_b, r_b))
     return res
 
 
@@ -98,8 +90,9 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
     used[known_ind1] = True
     used[known_ind2] = True
 
-    good_for_pnp = get_neighbours(known_ind1, frame_count) + get_neighbours(known_ind2, frame_count)
+    good_for_pnp = get_neighbours(known_ind1, frame_count) | get_neighbours(known_ind2, frame_count)
 
+    ## MAIN LOOP ##
     while True:
         best_for_pnp = -1
         inliers_for_best = None
@@ -108,14 +101,16 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
         ind1_for_best = None
         ind2_for_best = None
         corn_for_best = None
+        outliers_for_best = None
 
         for i in good_for_pnp:
             if used[i]:
                 continue
 
             corn = corner_storage[i]
-            inds, (ind1, ind2) = snp.intersect(point_cloud_builder.ids.flatten(), corn.ids.flatten(), indices=True)
-            
+            inds, (ind1, ind2) = snp.intersect(point_cloud_builder.ids.flatten(), 
+                                               corn.ids.flatten(), indices=True)
+
             if len(inds) < 4:
                 continue
 
@@ -139,6 +134,10 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                     ind2_for_best = ind2
                     corn_for_best = corn
 
+                    mask = np.zeros(len(inds), dtype=bool)
+                    mask[inliers.flatten()] = True
+                    outliers_for_best = inds[~mask]
+
                 elif len(inliers_for_best) < len(inliers):
                     inliers_for_best = inliers
                     r_vec_for_best = r_vec
@@ -148,11 +147,13 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                     ind2_for_best = ind2
                     corn_for_best = corn
 
-        #print(best_for_pnp)
+                    mask = np.zeros(len(inds), dtype=bool)
+                    mask[inliers.flatten()] = True
+                    outliers_for_best = inds[~mask]
+
         if best_for_pnp == -1:
-            print("WTF")
-            break
-            
+            break  
+
         succ, r_vec, t_vec = cv2.solvePnP(
             point_cloud_builder.points[ind1_for_best[inliers_for_best.flatten()]],
             corn_for_best.points[ind2_for_best[inliers_for_best.flatten()]],
@@ -172,13 +173,15 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
         best_pts3d = None
         best_ids = None
         cos_med_best = 1
-        for j in get_neighbours_without_near_range(best_for_pnp, frame_count):
+        ns = get_neighbours(best_for_pnp, frame_count)
+        for j in ns:
             if not used[j]:
                 continue
-            if not check_baseline(view_mats[frame1], view_mats[i], base_line_min_dist):
+            
+            if not check_baseline(view_mats[j], view_mats[best_for_pnp], baseline_min_dist):
                 continue
 
-            correspondence = build_correspondences(corner_storage[best_for_pnp], corner_storage[j])
+            correspondence = build_correspondences(corner_storage[best_for_pnp], corner_storage[j], outliers_for_best)
             pts3d, ids, cos_med = triangulate_correspondences(correspondence,
                                                                 view_mats[best_for_pnp],
                                                                 view_mats[j],
@@ -188,9 +191,10 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
             if cos_med < cos_med_best:
                 best_pts3d = pts3d
                 best_ids = ids
-        
-        point_cloud_builder.add_points(best_ids, best_pts3d)
-        good_for_pnp += get_neighbours(best_for_pnp, frame_count)
+        if best_pts3d is not None:
+            point_cloud_builder.add_points(best_ids, best_pts3d)
+
+        good_for_pnp |= ns
         used[best_for_pnp] = True
         
 
